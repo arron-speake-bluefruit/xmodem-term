a20261c3359168b558d7c9123b8c1ef4df112935
Arron Speake <arron.speake@bluefruit.co.uk>
2021-01-20 11:17:31 +0000

    Add clap interface.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::clap_app;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(app =>
+        (name: env!("CARGO_PKG_NAME"))
+        (version: env!("CARGO_PKG_VERSION"))
+        (author: env!("CARGO_PKG_AUTHORS"))
+        (about: env!("CARGO_PKG_DESCRIPTION"))
+        (@setting ArgRequiredElseHelp)
+        (@setting DisableHelpSubcommand)
+        (@setting GlobalVersion)
+        (@setting StrictUtf8)
+        (@setting StrictUtf8)
+        (@arg device: +required "The device to use for xmodem transfer.")
+        (@arg file: +required "The file to be transferred.")
+    )
+    .get_matches();
+    println!("{:?}", matches);
 }
