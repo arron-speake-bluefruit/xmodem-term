d76b5340b659d388975ff2674611c739d095b991
Arron Speake <arron.speake@bluefruit.co.uk>
2021-01-29 12:00:15 +0000

    Refactor parser usage.

--- src/bin/xmodem-term.rs
+++ src/bin/xmodem-term.rs
@@ -5,9 +5,9 @@ use serial::{BaudRate, CharSize, FlowControl, Parity, StopBits};
 
 // TODO: Maybe macro_rules! these?
 
-fn get_baud_rate(name: &str) -> Result<BaudRate, String> {
+fn get_baud_rate(name: String) -> Result<BaudRate, String> {
     use BaudRate::*;
-    match name {
+    match name.as_ref() {
         "110" => Ok(Baud110),
         "300" => Ok(Baud300),
         "600" => Ok(Baud600),
@@ -19,47 +19,47 @@ fn get_baud_rate(name: &str) -> Result<BaudRate, String> {
         "38400" => Ok(Baud38400),
         "57600" => Ok(Baud57600),
         "115200" => Ok(Baud115200),
-        _ => Err(format!("Invalid baud rate of {}.", name)),
+        _ => Err(name),
     }
 }
 
-fn get_char_size(name: &str) -> Result<CharSize, String> {
+fn get_char_size(name: String) -> Result<CharSize, String> {
     use CharSize::*;
-    match name {
+    match name.as_ref() {
         "5" => Ok(Bits5),
         "6" => Ok(Bits6),
         "7" => Ok(Bits7),
         "8" => Ok(Bits8),
-        _ => Err(format!("Invalid char size of {}.", name)),
+        _ => Err(name),
     }
 }
 
-fn get_parity(name: &str) -> Result<Parity, String> {
+fn get_parity(name: String) -> Result<Parity, String> {
     use Parity::*;
-    match name {
+    match name.as_ref() {
         "even" => Ok(ParityEven),
         "none" => Ok(ParityNone),
         "odd" => Ok(ParityOdd),
-        _ => Err(format!("Invalid parity of {}.", name)),
+        _ => Err(name),
     }
 }
 
-fn get_stop_bits(name: &str) -> Result<StopBits, String> {
+fn get_stop_bits(name: String) -> Result<StopBits, String> {
     use StopBits::*;
-    match name {
+    match name.as_ref() {
         "1" => Ok(Stop1),
         "2" => Ok(Stop2),
-        _ => Err(format!("Invalid stop bits of {}.", name)),
+        _ => Err(name),
     }
 }
 
-fn get_flow_control(name: &str) -> Result<FlowControl, String> {
+fn get_flow_control(name: String) -> Result<FlowControl, String> {
     use FlowControl::*;
-    match name {
+    match name.as_ref() {
         "hardware" => Ok(FlowHardware),
         "none" => Ok(FlowNone),
         "software" => Ok(FlowSoftware),
-        _ => Err(format!("Invalid stop bits of {}.", name)),
+        _ => Err(name),
     }
 }
 
@@ -74,19 +74,31 @@ fn main() -> Result<(), String> {
         (@setting GlobalVersion)
         (@setting StrictUtf8)
         (@arg baud_rate: -b --baudrate +takes_value default_value("115200")
-            "The Baud rate of the serial.")
+            validator(|b| get_baud_rate(b).map(|_| ()))
+            "The Baud rate of the serial."
+        )
         (@arg char_size: -c --charsize +takes_value default_value("8")
-            "The number of bits per character.")
+            validator(|b| get_char_size(b).map(|_| ()))
+            "The number of bits per character."
+        )
         (@arg parity: -p --parity +takes_value default_value("none")
-            "The parity checking mode.")
+            validator(|b| get_parity(b).map(|_| ()))
+            "The parity checking mode."
+        )
         (@arg stop_bits: -s --stopbits +takes_value default_value("1")
-            "The number of stop bits transmitted after every character.")
+            validator(|b| get_stop_bits(b).map(|_| ()))
+            "The number of stop bits transmitted after every character."
+        )
         (@arg flow_control: -f --flowcontrol +takes_value default_value("none")
-            "The serial flow control mode.")
+            validator(|b| get_flow_control(b).map(|_| ()))
+            "The serial flow control mode."
+        )
         (@arg device: +required
-            "The device to use for xmodem transfer.")
+            "The device to use for xmodem transfer."
+        )
         (@arg file: +required
-            "The file to be transferred.")
+            "The file to be transferred."
+        )
     )
     .get_matches();
 
@@ -95,13 +107,20 @@ fn main() -> Result<(), String> {
     let device_path = matches.value_of("device").unwrap();
     let file_path = matches.value_of("file").unwrap();
 
+    fn unwrap_argument_parser<T>(
+        arg: Option<&str>,
+        parser: fn(String) -> Result<T, String>,
+    ) -> T {
+        parser(arg.unwrap().to_owned()).unwrap()
+    }
+
     let device = setup_device(
         device_path,
-        get_baud_rate(matches.value_of("baud_rate").unwrap())?,
-        get_char_size(matches.value_of("char_size").unwrap())?,
-        get_parity(matches.value_of("parity").unwrap())?,
-        get_stop_bits(matches.value_of("stop_bits").unwrap())?,
-        get_flow_control(matches.value_of("flow_control").unwrap())?,
+        unwrap_argument_parser(matches.value_of("baud_rate"),    get_baud_rate),
+        unwrap_argument_parser(matches.value_of("char_size"),    get_char_size),
+        unwrap_argument_parser(matches.value_of("parity"),       get_parity),
+        unwrap_argument_parser(matches.value_of("stop_bits"),    get_stop_bits),
+        unwrap_argument_parser(matches.value_of("flow_control"), get_flow_control),
     )?;
 
     let file = File::open(file_path).map_err(|e| format!("Failed to open file: {}.", e))?;
